605feff2630e739388901dfca00201cea3ecfa02
eulalourens00 <kocetovaanastasia19@gmail.com>
2025-02-25 21:55:53 +0300

    исключения

---
+++ Exceprtions/MyExp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceprtions
+{
+    internal class MyExp:ApplicationException
+    {
+        private string _message;
+        public DateTime TimeException { get; private set; }
+
+        public MyExp()
+        {
+            _message = "My exception!";
+            TimeException = DateTime.Now;
+        }
+        public override string Message
+        {
+            get { return _message; }
+        }
+    }
+
+    internal class MyException : ApplicationException { 
+        public DateTime TimeException { get; private set; }
+        public MyException() : base("MY EXCEPTION!!")
+        {
+            TimeException = DateTime.Now;
+        } 
+    }
+}
---
+++ Exceprtions/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Exceprtions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //try {
+            //    int x = 5;
+            //    int y = x / 0;
+            //    Console.WriteLine($"Result: {y}");
+            //}
+            //catch { }
+            //MyExp exp = new MyExp();
+            //Console.WriteLine(exp.Message);
+            Console.Write("Enter two numbers: ");
+            string[] numbers = Console.ReadLine().Split();
+            int num1 = int.Parse(numbers[0]);
+            int num2 = int.Parse(numbers[1]);
+            int res = 0;
+
+            try
+            {
+                res = num1 / num2;
+                if (res % 2 != 0)
+                {
+                    throw new MyException();
+                }
+                Console.WriteLine($"Result: {res}");
+            }
+            catch (MyException my)
+            {
+                Console.WriteLine(my.Message);
+                Console.WriteLine(my.TimeException);
+            }
+
+            catch(Exception ex) // база класса, должен быть всегда в конце
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}

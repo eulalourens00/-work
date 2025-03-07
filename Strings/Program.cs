1ada4dfcf67da58cf7422323ebab3fdc2c80b28d
eulalourens00 <kocetovaanastasia19@gmail.com>
2025-03-07 13:33:16 +0300

    07.03.2025 new 15pr

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Strings
+﻿using System.Diagnostics;
+
+namespace Strings
 {
     internal class Program
     {
@@ -136,25 +138,157 @@
 
             Console.WriteLine($"Two same symbols is - '{same}'");
         }
+        //___________________________________________________________________________________________________
+        //      ПРАКТИЧЕСКАЯ 15
+        //1.
+        static void NewWord()
+        {
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine().ToLower();
+            string newword = string.Empty;
+
+            for(int i = 1; i < sentence.Length; i++)
+            {
+                if(i % 2 != 0) { newword += sentence[i]; }
+            }
+
+            Console.WriteLine("New word is {0}", newword);
+        }
+        //2.
+        static void Symbols()
+        {
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine().ToLower();
+            string sub = "+";
+            string sub2 = "*";
+
+            int countPlus = 0;
+            int countStar = 0;
+
+            int i = 0;
+            while ((i = sentence.IndexOf(sub, i)) != -1)
+            { countPlus++; i += sub.Length; }
+
+            int j = 0;
+            while ((j = sentence.IndexOf(sub2, j)) != -1)
+            { countStar++; j += sub2.Length; }
+            
+            Console.WriteLine($"Count of + is {countPlus} \nCount of * is {countStar}");
+        }
+        //3
+        static void Zapytaya()
+        {
+            string sentence = "To be or not to be, that is the question.";
+            string start = string.Empty;
+
+            int zap = sentence.IndexOf(',');
+            if (zap != -1) {
+                string result = sentence.Substring(0, zap);
+                Console.WriteLine("Start: {0} ", sentence);
+                Console.WriteLine("Before: {0} ", result);
+            }
+            else { Console.WriteLine("No zapyataya"); }
+        }
+        //4
+        static void OoooO()
+        {
+            Console.Write("Sentence: ");
+            string sentence = Console.ReadLine().ToLower().Trim();
+            string sub = "-";
+            int i = 0;
+            int countsub = 0;
+            while ((i = sentence.IndexOf(sub, i)) != -1)
+            { countsub++; i += sub.Length; }
+
+            if (countsub > 0) { Console.WriteLine("Invalid input."); }
+            else
+            {
+                int count_O = 0;
+                int j = 0;
+                string o = "o";
+                string[] new_sentence = sentence.Split( );
+                while ((j = new_sentence[0].IndexOf(o, j)) != -1) { count_O++; j += o.Length; }
+                Console.WriteLine($"Count of 'O' in the first word = {count_O}");
+            }
+        }
+        //5
+        static void Sentences()
+        {
+            Console.WriteLine("Text (dont forget about the dots!): ");
+            string text = Console.ReadLine().Trim();
+            string[] dots = text.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine($"Count of sentences = {dots.Length}");
+        }
+        //6
+        static void E_up_I()
+        {
+            Console.Write("Text: ");
+            string text = Console.ReadLine().Trim().ToLower();
+            text = text.Replace("e", "i");
+            Console.WriteLine(text);
+        }
+        //7
+        static void Ah_up_Uh()
+        {
+            Console.Write("Text: ");
+            string text = Console.ReadLine().Trim().ToLower();
+            text = text.Replace("ah", "uh");
+            Console.WriteLine(text);
+        }
+        //8
+        static void ReplaceMiddle()
+        {
+            Console.Write("Text: ");
+            string text = Console.ReadLine().Trim().ToLower();
+            string result = string.Empty;
+            if(text.Length == 12)
+            {
+                string part = text.Substring(2, 8); //3, 9
+                char[] strArray = part.ToCharArray();
+                Array.Reverse(strArray);
+                result = new string(strArray);
+               
+                Console.WriteLine($"Result: {text.Substring(0, 2)}{result}{text.Substring(9)}");
+            }
+            else { Console.WriteLine("Invalid input."); }
+        }
         static void Main(string[] args)
         {
-            //1.
-            CountOfWords();
-            //2.
-            Reverse();
-            //3.
-            Vowel();
-            //4.
-            SubStrCount();
-            //5.
-            CountSpace();
-            //6.
-            CountSymbols();
-            //7.
-            DifferentSymb();
-            ////8.
-            TwoSame();
+                        //// №14
+            ////1.
+            //CountOfWords();
+            ////2.
+            //Reverse();
+            ////3.
+            //Vowel();
+            ////4.
+            //SubStrCount();
+            ////5.
+            //CountSpace();
+            ////6.
+            //CountSymbols();
+            ////7.
+            //DifferentSymb();
+            //////8.
+            //TwoSame();
 
+                        ////№15
+            ////1.
+            //NewWord();
+            ////2.
+            //Symbols();
+            ////3.
+            //Zapytaya();
+            ////4.
+            //OoooO();
+            ////5.
+            //Sentences();
+            ////6.
+            //E_up_I();
+            ////7.
+            //Ah_up_Uh();
+            //8.
+            //ReplaceMiddle();
 
             //--------------------------------------------------------------------------
             //  CLASSWORK

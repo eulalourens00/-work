05ed055943e0fa8ac7a6a9c6550e6d9c0460a697
eulalourens00 <kocetovaanastasia19@gmail.com>
2025-03-10 11:14:52 +0300

    10.03.2025 15prac

---
+++ MassiveStrings/Program.cs
@@ -0,0 +1,90 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+using System.Diagnostics.Metrics;
+
+namespace MassiveStrings
+{
+    internal class Program
+    {
+        //1.
+        static void LastNames()
+        {
+            Console.WriteLine("\tLast names: ");
+            string names = Console.ReadLine();
+            string[] lastnames = names.Split(',');
+            string max = string.Empty;
+
+            foreach (string name in lastnames) 
+            {
+                if(name.Length > max.Length) {  max = name; }
+            }
+            Console.WriteLine($"The longest last name is {max}");
+        }
+        //2.
+        static void Choises()
+        {
+            List<Candidates> voters = new List<Candidates>
+            {
+                new Candidates("Leeky Suya", "GMS Street 5", new DateTime(2007, 12, 27)),
+                new Candidates("Stepano Step", "Boor Street 7A", new DateTime(2005, 10, 18)),
+                new Candidates("Poraky Leo", "Lesopol Street 12", new DateTime(1998, 8, 12)),
+                new Candidates("Anna Smith", "GMS Street 5", new DateTime(2000, 5, 15)),
+                new Candidates("John Doe", "Boorew Street 11C", new DateTime(1990, 3, 22))
+            };
+
+            var NeedVoters = voters.Where(voter => voter.Age() >= 18).ToList();
+
+            NeedVoters.Sort((x, y) => string.Compare(x.Address, y.Address));
+
+            Console.WriteLine("\tCandidates:");
+            foreach (var voter in NeedVoters)
+            {
+                Console.WriteLine($"{voter.FullName}\n Address: {voter.Address}, Age: {voter.Age()}");
+            }
+        }
+
+        //3.
+        static void DSumma()
+        {
+            Console.Write("Last number: ");
+            Int32.TryParse(Console.ReadLine(), out int last);
+            int summa = 0;
+            for(int i = 1; i <= last; i++)
+            {
+                summa += i;
+                Console.Write($"d{i} ");
+            }
+            Console.WriteLine($"\nSumma = {summa}");
+        }
+        //4.
+        static void TextNumber()
+        {
+            Console.WriteLine("Text:");
+            string Text = Console.ReadLine().Trim();
+            int summa = 0;
+            string[] digits = Regex.Split(Text, @"\D+");
+
+            foreach (string value in digits)
+            {
+                int number;
+                if (int.TryParse(value, out number))
+                {
+                    Console.Write("Numbers: ", value + " ");
+                    summa += number;
+                }
+            }
+            Console.WriteLine($"\nResult of numbers in the text: {summa}");
+        }
+        static void Main(string[] args)
+        {
+            //1
+            LastNames();
+            //2
+            Choises();
+            //3
+            DSumma();
+            //4
+            TextNumber();
+        }
+    }
+}
---
+++ MassiveStrings/candidates.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassiveStrings
+{
+    internal class Candidates
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public DateTime birthDate { get; set; }
+
+        public Candidates(string fullname, string address, DateTime birthday)
+        {
+            FullName = fullname;
+            Address = address;
+            birthDate = birthday;
+        }
+        DateTime currentDate = DateTime.Now;
+        public int Age()
+        {
+            int age = currentDate.Year - birthDate.Year;
+            if (currentDate.Month < birthDate.Month || (currentDate.Month == birthDate.Month && currentDate.Day < birthDate.Day))
+            {
+                age--;
+            }    
+            return age;            
+        }
+    }
+}

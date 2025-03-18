cec2f372a78a2b08e8aaafe7f42f21ca6fc880a0
eulalourens00 <kocetovaanastasia19@gmail.com>
2025-03-18 22:06:15 +0300

    21.03.2025

---
+++ 21-Pract(Morse code)/Morse code.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_Pract_Morse_code_
+{
+    internal class Morse_code
+    {
+        public Dictionary<char, string> Code = new Dictionary<char, string>()
+        {
+            {'A', ".-" },
+            {'B', "-..." },
+            {'C', "-.-." },
+            {'D', "-.." },
+            {'E', "." },
+            {'F', "..-." },
+            {'G', "--." },
+            {'H', "...." },
+            {'I', ".." },
+            {'J', ".---" },
+            {'K', "-.-" },
+            {'L', ".-.." },
+            {'M', "--" },
+            {'N', "-." },
+            {'O', "---" },
+            {'P', ".--." },
+            {'Q', "--.-" },
+            {'R', ".-." },
+            {'S', "..." },
+            {'T', "-" },
+            {'U', "..-" },
+            {'V', "...-" },
+            {'W', ".--" },
+            {'X', "-..-" },
+            {'Y', "-.--" },
+            {'Z', "--.." },
+
+            {'0', "-----" },
+            {'1', ".----" },
+            {'2', "..---" },
+            {'3', "...--" },
+            {'4', "....-" },
+            {'5', "....." },
+            {'6', "-...." },
+            {'7', "--..." },
+            {'8', "---.." },
+            {'9', "----." },
+
+            {'.', ".-.-.-" },
+            {',', "--..--" },
+            {'?', "..--.." },
+            {'`', ".----." },
+            {'!', "-.-.--" },
+            {'/', "-..-." },
+            {'(', "-.-." },
+            {')', "-.--.-" },
+            {'&', ".-..." },
+            {':', "---..." },
+            {';', "-.-.-." },
+            {'=', "-...-" },
+            {'+', ".-.-." },
+            {'-', "-....-" },
+            {'_', "..--.-" },
+            {'$', "...-..-" },
+            {'@', ".--.-." }
+            
+        };
+
+        public void ConvertToMorse(Dictionary<char, string>Code)
+        {
+            Console.WriteLine("Text To convert: ");
+            string text = Console.ReadLine().ToUpper().Trim();
+            char[] characters = text.ToCharArray();
+            string result = string.Empty;
+            foreach (char ch in text)
+            {
+                if (Code.ContainsKey(ch))
+                { result += Code[ch] + " ";}
+
+                else if (ch == ' ')
+                { result += " ";}
+
+                else
+                { result += "";}
+            }
+            Console.WriteLine($"Morse code: {result}");
+        }
+
+        public void ConvertMorseToTheText(Dictionary<char, string> Code)
+        {
+            Console.WriteLine("Code To convert: ");
+            string codee = Console.ReadLine().ToUpper().Trim();
+            string[] code = codee.Split();
+            //char[] characters = code.ToCharArray();
+            string result = string.Empty;
+            foreach (string str in code)
+            {
+                if (Code.ContainsValue(str))
+                {
+                    char character = Code.FirstOrDefault(x => x.Value == str).Key;
+                    result += character;
+                }
+                else if (string.IsNullOrEmpty(str))
+                { result += " "; }
+                else
+                { result += ""; }
+            }
+            Console.WriteLine($"Text: {result}");
+        }
+    }
+}
---
+++ 21-Pract(Morse code)/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _21_Pract_Morse_code_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Morse_code morse = new Morse_code();
+            morse.ConvertToMorse(morse.Code);
+            morse.ConvertMorseToTheText(morse.Code);
+
+        }
+    }
+}

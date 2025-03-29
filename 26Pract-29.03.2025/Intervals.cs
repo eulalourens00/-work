c8f676beac72730520486ca5d98524abc05d8d39
eulalourens00 <kocetovaanastasia19@gmail.com>
2025-03-29 14:34:18 +0300

    29.03.2025

---
+++ 26Pract-29.03.2025/Intervals.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26Pract_29._03._2025
+{
+    //1.
+    internal class IntervalSet
+    {
+        private int[][] _intervals;
+
+        public IntervalSet(int[][] intervals)
+        { _intervals = intervals; }
+
+        public int MinSetSize()
+        {
+            Array.Sort(_intervals, (a, b) => a[0].CompareTo(b[0]));
+
+            int count = 0;
+            int lastEnd = -1;
+
+            foreach (var interval in _intervals)
+            {
+                int start = interval[0];
+                int end = interval[1];
+
+                if (start > lastEnd)
+                {
+                    count += 2;
+                    lastEnd = end;
+                }
+                else
+                {
+                    if (lastEnd < end)
+                    {
+                        count += 1;
+                        lastEnd = end;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+
+    //2.
+    internal class MinimalDiap
+    {
+        private int[][] _intervals;
+
+        public MinimalDiap(int[][] intervals)
+        { _intervals = intervals; }
+
+        public void MinimalMaxValue()
+        {
+            Array.Sort(_intervals, (a, b) => a[0].CompareTo(b[0]));
+
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            foreach (var interval in _intervals)
+            {
+                if (interval[0] < min) { min = interval[0]; }
+                if (interval[1] > max) { max = interval[1]; }
+            }
+
+            Console.WriteLine($"Max of this - {max} \nMin of this - {min}");
+        }
+    }
+}
--- 26Pract-29.03.2025/Program.cs
+++ 26Pract-29.03.2025/Program.cs
@@ -5,7 +5,93 @@ namespace _26Pract_29._03._2025
     {
         static void Main(string[] args)
         {
+            //1.
+            int[][] intervals = new int[][]
+            {
+                new int[] { 1, 3 },
+                new int[] { 3, 7 },
+                new int[] { 8, 9 }
+            };
 
+            IntervalSet intervalSet = new IntervalSet(intervals);
+            int result = intervalSet.MinSetSize();
+            Console.WriteLine($"Answer is {result}");
+
+            //2.
+            int[][] massive = new int[][]
+            {
+                new int[] { 1, 3 },
+                new int[] { 2, 5 },
+                new int[] { 4, 8 },
+                new int[] { 6, 10 }
+            };
+
+            MinimalDiap minimalDiap = new MinimalDiap(massive);
+            minimalDiap.MinimalMaxValue();
+
+            //3.
+            CountOfWords();
+            //4.
+            Reverse();
+            //5.
+            Vowel();
+            //6.
+            SubStrCount();
+        }
+        //3.
+        static void CountOfWords()
+        {
+            Console.Write("Enter a sentence: ");
+            string val = Console.ReadLine();
+            int count = 0;
+            string[] strArr = val.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string str in strArr) { count++; }
+            Console.WriteLine("Count of words is {0}", count);
+        }
+        //4.
+        static void Reverse()
+        {
+            Console.Write("Enter a sentence: ");
+            string[] originalArray = Console.ReadLine().Split();
+            string[] reversedArray = new string[originalArray.Length];
+
+            int j = originalArray.Length;
+
+            for (int u = 0; u < j; u++)
+            {
+                char[] wordArray = originalArray[u].ToCharArray();
+                Array.Reverse(wordArray);
+                reversedArray[u] = new string(wordArray);
+            }
+            Console.WriteLine(string.Join(' ', reversedArray));
+        }
+        //5.
+        static void Vowel()
+        {
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine().ToLower();
+            char[] ch_vowel = { 'a', 'e', 'i', 'o', 'u', 'y' };
+            int vowel_count = 0;
+            foreach (char vowel in ch_vowel)
+            {
+                int i = 0;
+                while ((i = sentence.IndexOf(vowel, i)) != -1) { vowel_count++; i++; }
+            }
+            Console.WriteLine("Count of vowels in your sentence is {0}", vowel_count);
+        }
+        static void SubStrCount()
+        {
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine().ToLower();
+            Console.Write("Enter a sub string: ");
+            string sub = Console.ReadLine().ToLower();
+            int count = 0;
+            int i = 0;
+            while ((i = sentence.IndexOf(sub, i)) != -1)
+            {
+                if (sentence.IndexOf(sub) > 1) { count++; i += sub.Length; }
+            }
+            Console.WriteLine($"Count of your word {sub.ToUpper()} is {count}");
         }
     }
 }

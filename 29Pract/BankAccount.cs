8f40e71d5d8a07183c8b5a916d74d889362bf1c6
Leeky[Suya] <kocetovaanastasia19@gmail.com>
2025-04-13 20:34:06 +0300

    13.04.2025
    strings void

---
+++ 29Pract/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29Pract
+{
+    public class BankAccount
+    {
+        private int IdAccount;
+        private double Balance;
+        private Type _Type;
+
+        public enum Type { Credit, Deposit, Funded}
+        public BankAccount() { }
+
+        public void AddData()
+        {
+            Console.Write("Balance: ");
+            Double.TryParse(Console.ReadLine(), out Balance);
+            Console.Write("Type (Credit, Deposit, Funded) - 1, 2, 3: ");
+            Int32.TryParse(Console.ReadLine(), out int type);
+            switch (type)
+            {
+                case 1: _Type = Type.Credit; break;
+                case 2: _Type = Type.Deposit; break;
+                case 3: _Type = Type.Funded; break;
+                default: throw new ArgumentException("Wrong case.");
+            }
+            Console.Write("Id of account: ");
+            Int32.TryParse(Console.ReadLine(), out IdAccount);
+        }
+        public override string ToString() {
+            return $"{IdAccount} - {_Type}\nAmount: {Balance}";
+        }
+
+        public void Plus()
+        {
+            Console.Write($"Balance: {Balance}");
+            Console.Write("\nAmount to plus: ");
+            Double.TryParse(Console.ReadLine(), out double amount);
+            if(amount > 0) { Balance += amount; }
+            else { throw new InvalidDataException(); }
+            Console.Write($"Updated balance: {Balance}");
+        }
+        public void Minus()
+        {
+            Console.Write($"Balance: {Balance}");
+            Console.Write("\nAmount to minus: ");
+            Double.TryParse(Console.ReadLine(), out double amount);
+            if (amount > 0 && Balance >= amount) { Balance -= amount; }
+            else { throw new InvalidDataException(); }
+            Console.Write($"Updated balance: {Balance}");
+        }
+    }
+}
---
+++ 29Pract/Program.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.VisualBasic;
+
+namespace _29Pract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            BankAccount account = new BankAccount();
+            account.AddData();
+            Console.WriteLine(account.ToString());
+            account.Plus();
+
+            Console.WriteLine();
+
+            BankAccount2 account2 = new BankAccount2();
+            account2.AddData();
+            Console.WriteLine(account2.ToString());
+            account2.Minus();
+            //2
+            int[] array = { 5, 3, 8, 6, 1, 9, 2, 7, 4 };
+            BubbleSortAlgorithm(array);
+            //3
+            string[] strings = { "la", "lalala", "lel", "lelelele", "l", "luuu" };
+            FastSortAlgorithm(strings);
+            //4
+            FindLength();
+            //5
+            PrintLast();
+            //6
+            CountWords();
+            //7
+            CompareWords();
+            //8
+            CountEverything();
+            //9
+            CopyTo();
+            //10
+            string[] str = { "meow", "me", "meoooow", "mememme" };
+            FastSortAlgorithm(str);
+        }
+        //2
+        static void BubbleSortAlgorithm(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            foreach (var num in arr) {  Console.Write(num + " ");  }
+        }
+        //3.
+        static void FastSortAlgorithm(string[] str)
+        {
+            int n = str.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (str[j].Length > str[j + 1].Length)
+                    {
+                        string temp = str[j];
+                        str[j] = str[j + 1];
+                        str[j + 1] = temp;
+                    }
+                }
+            }
+            foreach(var num in str) {  Console.Write(num + " "); }
+        }
+        //4.
+        static void FindLength()
+        {
+            Console.Write("Word: ");
+            string word = Console.ReadLine();
+            char[] chars = word.ToCharArray();
+            int i = 0;
+            foreach (char c in chars) { i++; }
+            Console.WriteLine($"Length - {i}");
+        }
+        //5
+        static void PrintLast()
+        {
+            Console.Write("Word: ");
+            string word = Console.ReadLine();
+            char[] chars = word.ToCharArray();
+            Array.Reverse(chars);
+            foreach(var c in chars) { Console.Write(c + " "); }
+        }
+        //6
+        static void CountWords()
+        {
+            Console.WriteLine("Sentence: ");
+            string[] text = Console.ReadLine().Split();
+            Console.WriteLine($"Count of words: {text.Count()}");
+        }
+        //7
+        static void CompareWords()
+        {
+            Console.WriteLine("Enter two sentenses:");
+            string[] strings = Console.ReadLine().Split('.');
+            if (strings[0].Length > strings[1].Length) { Console.WriteLine("The first is more."); }
+            else if (strings[0].Length == strings[1].Length) { Console.WriteLine("The same length."); }
+            else { Console.WriteLine("The second is more."); }
+        }
+        //8
+        static void CountEverything()
+        {
+            Console.WriteLine("Sentence:");
+            string input = Console.ReadLine();
+            int alphabets = 0;
+            int digits = 0;
+            int specialChars = 0;
+
+            foreach (char ch in input)
+            {
+                if (char.IsLetter(ch))
+                {  alphabets++;}
+                else if (char.IsDigit(ch))
+                {  digits++; }
+                else
+                {  specialChars++; }
+            }
+
+            Console.WriteLine($"Alphabets: {alphabets}");
+            Console.WriteLine($"Digits: {digits}");
+            Console.WriteLine($"Special symbols: {specialChars}");
+        }
+        //9
+        static void CopyTo()
+        {
+            Console.WriteLine("Sentence for copy:");
+            string sourceString = Console.ReadLine();
+            char[] charArray = sourceString.ToCharArray();
+            string copiedString = new string(charArray);
+
+            Console.WriteLine($"Before: \"{sourceString}\"");
+            Console.WriteLine($"After: \"{copiedString}\"");
+        }
+    }
+}

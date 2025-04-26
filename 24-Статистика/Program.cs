db7e7597899558555bdebf45e59c33ed9e152470
Leeky[Suya] <kocetovaanastasia19@gmail.com>
2025-04-26 18:08:11 +0300

    25.04.2025

--- 24-Статистика/Program.cs
+++ 24-Статистика/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = "Вот дом, Который построил Джек. А это пшеница, Которая в темном чулане хранится В доме, Который построил Джек. А это веселая птица синица, Которая часто ворует пшеницу, Которая в темном чулане хранится В доме, Который построил Джек.";
+            var textNow = new string(text.Where(c => !char.IsPunctuation(c)).ToArray()).ToLower();
+
+            string[] words = textNow.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (result.ContainsKey(word))
+                {  result[word]++; }
+                else
+                {  result.Add(word, 1); }
+            }
+            Console.WriteLine("Слово\t\tКоличество");
+            Console.WriteLine("-----------------------");
+            foreach (var pair in result.OrderByDescending(pair => pair.Value))
+            {  Console.WriteLine($"{pair.Key}\t\t{pair.Value}"); }
         }
     }
 }
